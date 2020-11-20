f074db0b8df351184081ddd34d0837bd2002121b
shinshin <shinshin@dummy.com>
2020-11-20 17:22:02 +0900

    BeWebApi01 uses Azure AD Authentication for DbConnection

--- BeWebApi01/DbContext/DbContext.cs
+++ BeWebApi01/DbContext/DbContext.cs
@@ -8,11 +8,12 @@ namespace BeWebApi01.Context
         public DatabaseContext(DbContextOptions<DatabaseContext> options)
             : base(options)
         {
+            var conn = (Microsoft.Data.SqlClient.SqlConnection)Database.GetDbConnection();
+            conn.AccessToken = (new Microsoft.Azure.Services.AppAuthentication.AzureServiceTokenProvider()).GetAccessTokenAsync("https://database.windows.net/").Result;
         }
         public DbSet<Accident> accident { get; set; }
         public DbSet<TodoItem> todoItem { get; set; }
         //public DbSet<Owner> Owner { get; set; }
         //public DbSet<Vehicle> Vehicle { get; set; }
-
     }
 }
